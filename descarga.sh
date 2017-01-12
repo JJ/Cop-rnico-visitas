64121bca80695942f585963e68e30d734bffafa8
JJ Merelo <jjmerelo@gmail.com>
2017-01-12 07:26:49 +0100

    Actualiza ficheros

--- descarga.sh
+++ descarga.sh
@@ -2,5 +2,5 @@
 
 curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011000 | jq '.items[] | .views ' > 2017.dat
 curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011000 | jq '.items[] | .views ' > 2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011000 | jq '.items[] | .views ' > visitas-teoría-heliocéntrica-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011000 | jq '.items[] | .views ' > visitas-teoría-heliocéntrica-2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011100 | jq '.items[] | .views ' > visitas-teoría-heliocéntrica-2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011100 | jq '.items[] | .views ' > visitas-teoría-heliocéntrica-2016.dat
