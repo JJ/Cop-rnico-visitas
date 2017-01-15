3aefd12279d5541c360b79dcee105bf91f81a9ff
JJ Merelo <jjmerelo@gmail.com>
2017-01-15 09:02:59 +0100

    Actualiza datos

--- descarga.sh
+++ descarga.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011300 | jq '.items[] | .views ' > datos/2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011300 | jq '.items[] | .views ' > datos/2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011300 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011300 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/user/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011300 | jq '.items[] | .views ' > datos/user-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/user/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011300 | jq '.items[] | .views ' > datos/user-2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/user/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011300 | jq '.items[] | .views ' > datos/user-visitas-teoría-heliocéntrica-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/user/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011300 | jq '.items[] | .views ' > datos/user-visitas-teoría-heliocéntrica-2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011400 | jq '.items[] | .views ' > datos/2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011400 | jq '.items[] | .views ' > datos/2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011400 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011400 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
