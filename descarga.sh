3bd6f918478e998cc58110844bc1683e04605ffe
JJ Merelo <jjmerelo@gmail.com>
2017-01-17 08:10:27 +0100

    Ahora resulta que suben de nuevo las visitas...
    Esto es una montaña rusa.

--- descarga.sh
+++ descarga.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011400 | jq '.items[] | .views ' > datos/2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011400 | jq '.items[] | .views ' > datos/2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011400 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011400 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011600 | jq '.items[] | .views ' > datos/2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011600 | jq '.items[] | .views ' > datos/2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2017010100/2017011600 | jq '.items[] | .views ' > datos/2017-meryl.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2016010100/2016011600 | jq '.items[] | .views ' > datos/2016-meryl.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011600 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011600 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
