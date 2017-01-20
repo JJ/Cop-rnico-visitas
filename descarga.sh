1fc8ddb967cbabdcd33f311bc16f61cf6dca95ce
JJ Merelo <jjmerelo@gmail.com>
2017-01-20 07:38:42 +0100

    Updates up to January 19th

--- descarga.sh
+++ descarga.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011700 | jq '.items[] | .views ' > datos/2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011700 | jq '.items[] | .views ' > datos/2016.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2017010100/2017011700 | jq '.items[] | .views ' > datos/2017-meryl.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2016010100/2016011700 | jq '.items[] | .views ' > datos/2016-meryl.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011700 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
-curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011700 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2017010100/2017011900 | jq '.items[] | .views ' > datos/2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Nicol%C3%A1s_Cop%C3%A9rnico/daily/2016010100/2016011900 | jq '.items[] | .views ' > datos/2016.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2017010100/2017011900 | jq '.items[] | .views ' > datos/2017-meryl.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Meryl_Streep/daily/2016010100/2016011900 | jq '.items[] | .views ' > datos/2016-meryl.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2017010100/2017011900 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2017.dat
+curl https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents/Teor%C3%ADa_helioc%C3%A9ntrica/daily/2016010100/2016011900 | jq '.items[] | .views ' > datos/visitas-teoría-heliocéntrica-2016.dat
