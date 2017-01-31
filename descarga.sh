6969e34d1e15af693f062bc0a4528b8de2be2f87
JJ Merelo <jjmerelo@gmail.com>
2017-01-31 07:56:20 +0100

    Adds new data

--- descarga.sh
+++ descarga.sh
@@ -2,12 +2,12 @@
 
 api="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents"
 today=`date +"%Y%m%d"`
-req="daily/2017010100"
+req="daily/2017011500"
 oo="00"
 oldtoday=`date +"%m%d"`
-oldreq="daily/2016010100/2016"
+oldreq="daily/2016011500/2016"
 
-for i in "Nicol%C3%A1s_Cop%C3%A9rnico" "Meryl_Streep" "John_Hurt" "Teor%C3%ADa_helioc%C3%A9ntrica"
+for i in "Paloma_Chamorro" "Winona_Ryder" "John_Hurt" 
 do
     curl "$api/$i/$req/$today$oo" | jq '.items[] | .views ' > "datos/$i-2017.dat"
 
