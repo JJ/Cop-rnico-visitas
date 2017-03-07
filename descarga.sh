9d27d2ea2ecba7684d8b3037a92ceecb7fd20524
JJ Merelo <jjmerelo@gmail.com>
2017-03-07 19:32:26 +0100

    Last version of files

--- descarga.sh
+++ descarga.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 api="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/all-access/all-agents"
-today=`date +"%Y%m%d"`
-req="daily/2017011500"
-oo="00"
-oldtoday=`date +"%m%d"`
-oldreq="daily/2016011500/2016"
+req="daily/2017011500/2017021500"
+oldreq="daily/2016011500/2016021500"
 
 for i in "Paloma_Chamorro" "Winona_Ryder" "John_Hurt" 
 do
-    curl "$api/$i/$req/$today$oo" | jq '.items[] | .views ' > "datos/$i-2017.dat"
+    curl "$api/$i/$req" | jq '.items[] | .views ' > "datos/$i-2017.dat"
 
-    curl "$api/$i/$oldreq$oldtoday$oo" | jq '.items[] | .views ' > "datos/$i-2016.dat"
+    curl "$api/$i/$oldreq" | jq '.items[] | .views ' > "datos/$i-2016.dat"
 done    
